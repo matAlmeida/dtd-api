0f07fd41436a1a82341edfb2c88b15b17fd64229
Matheus Almeida <mat.almeida@live.com>
2020-08-01 03:20:08 -0300

    chore: use typeorm config with js file

---
+++ ormconfig.example.js
@@ -0,0 +1,24 @@
+const DATABASE = process.env.PGDATABASE;
+
+module.exports = {
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: DATABASE || "ilab_dev",
+  synchronize: "all",
+  logging: [
+    "error"
+  ],
+  entities: [
+    "./src/domains/**/entities/*.ts"
+  ],
+  migrations: [
+    "./src/database/migration/**/*.ts"
+  ],
+  cli: {
+    "entitiesDir": "src/domains",
+    "migrationsDir": "src/database/migration"
+  }
+}
