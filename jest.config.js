947a99f1036b7ef1c2d1b066c2ca6e5ff1c901ce
Matheus Almeida <mat.almeida@live.com>
2020-08-20 02:07:56 -0300

    chore: add coverage script

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,18 @@ module.exports = {
   clearMocks: true,
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
   preset: 'ts-jest',
+  collectCoverageFrom: [
+    'src/domains/**/*.ts',
+    '!src/domains/**/routes.ts',
+    'src/libs/**/*.ts',
+    'src/models/**/*.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    '/database/',
+    '/config/',
+    '.*\.controller\.ts',
+  ]
 }
