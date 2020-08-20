495b2d9017c6e739134eea1e21197059ddef8c62
Matheus Almeida <mat.almeida@live.com>
2020-08-20 02:18:15 -0300

    chore: add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,19 @@ module.exports = {
     '/database/',
     '/config/',
     '.*\.controller\.ts',
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+    "src/domains/**/use-cases/**/*.usecase.ts": {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  },
 }
