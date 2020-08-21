067a0329479f9d7c82d9e2da22fb54a080d9109d
Matheus Almeida <mat.almeida@live.com>
2020-08-20 22:22:14 -0300

    tets: add test to when user already exists

--- src/domains/users/use-cases/create-user/create-user.test.ts
+++ src/domains/users/use-cases/create-user/create-user.test.ts
@@ -2,6 +2,8 @@ import { getConnection, createConnection } from 'typeorm';
 
 import { dropDatabase } from '@libs/test-utils';
 
+import { BadRequestError } from '@models/error';
+
 import { DatabaseUserRepository } from '@domains/users/repositories/implementations/DatabaseUserRepository';
 
 import { createUserUseCase } from './index';
@@ -49,5 +51,17 @@ describe('Create User [Use Case]', () => {
     expect(createdUser.email).toEqual(testUser.email);
     expect(createdUser.password).not.toEqual(testUser.password);
     expect(createdUser.password_salt).toBeTruthy();
+  });
+
+  test('should fail when user with same email already exists', async () => {
+    expect.assertions(2);
+
+    try {
+      await createUserUseCase.execute(testUser);
+      await createUserUseCase.execute(testUser);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestError);
+      expect(error).toHaveProperty('message', 'User already exists.');
+    }
   });
 });
